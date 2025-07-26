2f2153a5792547d890a5441417b6afac450e855e
luth <88308177+luthpg@users.noreply.github.com>
2025-07-26 13:07:48 +0900

    rolldown.config.tsの出力ファイル名を動的に設定し、removeExportPluginを追加。index.cssにライトおよびダークモードのカラースキームを追加。

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'rolldown';
+import { removeExportPlugin } from 'rolldown-plugin-remove-export';
+
+const outputFileName = 'server.js'; // Htmlファイルと別名にする
 
 export default defineConfig({
   input: 'server/app.ts',
   output: {
-    file: 'dist/server.js',
+    file: `dist/${outputFileName}`,
     format: 'esm',
   },
+  plugins: [removeExportPlugin(outputFileName)],
 });
